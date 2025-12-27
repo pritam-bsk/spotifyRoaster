344f9d97054684ac9e19898b913c4a1b27430e34
pritam-bsk <riju.coder@gmail.com>
2025-12-27 21:13:54 +0530

    fix: implement spotify callback to handle token exchange

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,9 +17,31 @@ const userLogin = asyncHandler(async (req, res) => {
     res.redirect(`https://accounts.spotify.com/authorize?${params}`);
 });
 
-const spotifyCallback = asyncHandler((req,res)=>{
-    console.log(`####### callback #########`);
-    res.send(`####### callback #########`)
+const spotifyCallback = asyncHandler(async (req, res) => {
+    const code = req.query.code;
+    const tokenRes = await fetch(
+        "https://accounts.spotify.com/api/token",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Authorization":
+                    "Basic " +
+                    Buffer.from(
+                        process.env.SPOTIFY_CLIENT_ID +
+                        ":" +
+                        process.env.SPOTIFY_CLIENT_SECRET
+                    ).toString("base64"),
+            },
+            body: new URLSearchParams({
+                grant_type: "authorization_code",
+                code,
+                redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
+            }),
+        }
+    );
+    tokenData = await tokenRes.json();
+    res.send(tokenData);
 })
 
 export { userLogin, spotifyCallback };
