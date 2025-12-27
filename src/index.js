4d94d59aa3c7a3fb6153e15907d6451df2c163be
pritam-bsk <riju.coder@gmail.com>
2025-12-27 15:42:02 +0530

    recive callback

--- src/index.js
+++ src/index.js
@@ -4,13 +4,34 @@ import 'dotenv/config';
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.json({
         message: "Hello, World!",
         status: "success"
     });
 })
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on http://localhost:${PORT}`);
+const SCOPES = [
+  "user-top-read",
+  "user-read-recently-played",
+  "user-read-email",
+].join(" ");
+
+app.get('/login', (req, res) => {
+    const params = new URLSearchParams({
+        response_type: "code",
+        client_id: process.env.SPOTIFY_CLIENT_ID,
+        scope: SCOPES,
+        redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
+    });
+
+    res.redirect(`https://accounts.spotify.com/authorize?${params}`);
+})
+
+app.get('/api/v1/spotify-callback', (req, res) => {
+    console.log(req);
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${process.env.APP_URL}${PORT}`);
 });
