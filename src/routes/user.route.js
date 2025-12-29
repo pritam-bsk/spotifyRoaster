b88df2dbceb0c1685241af94f6b6e6619b267ca9
pritam-bsk <riju.coder@gmail.com>
2025-12-29 10:39:00 +0530

    add logout path

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { userLogin, userMe } from "../controllers/user.controller.js";
+import { logout,userLogin, userMe } from "../controllers/user.controller.js";
 import { authenticateUser } from '../middlewares/auth.middleware.js'
 
 const userRouter = Router();
 
 userRouter.route('/login').get(userLogin);
 userRouter.route('/me').get(authenticateUser, userMe);
+userRouter.route('/logout').post(logout);
 
 export default userRouter;
