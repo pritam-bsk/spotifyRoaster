58b45f080b30909be240934a764d1098a593a125
pritam-bsk <riju.coder@gmail.com>
2025-12-29 23:11:53 +0530

    show user from db

--- src/controllers/sendToDB.controller.js
+++ src/controllers/sendToDB.controller.js
@@ -2,13 +2,6 @@ import { asyncHandler } from "../utils/asyncHandler.util.js";
 import { User } from "../models/user.model.js";
 
 export const exportAllUser = asyncHandler(async (req, res) => {
-    const accessToken = req.accessToken || req.cookies.access_token;
-    if (!accessToken) {
-        const error = new Error("Access token not found. Please login.");
-        error.status = 401;
-        throw error;
-    }
-
     const users = await User.find({});
     return res.json({
         status: 200,
