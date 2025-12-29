1ff33d2e3af6d579f98f82938351e494269d35b0
pritam-bsk <riju.coder@gmail.com>
2025-12-29 13:35:24 +0530

    fix

--- src/controllers/gemini.controller.js
+++ src/controllers/gemini.controller.js
@@ -110,7 +110,7 @@ const generateRoast = asyncHandler(async (req, res) => {
     const roastJSON = buildRoaster(roastData);
     const prompt = genaratePromt(roastJSON)
     const resp = await fetch(
-        `https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+        `https://generativelanguage.googleapis.com/v1/models/gemini-2.5-pro:generateContent?key=${process.env.GEMINI_API_KEY}`,
         {
             method: "POST",
             headers: { "Content-Type": "application/json" },
